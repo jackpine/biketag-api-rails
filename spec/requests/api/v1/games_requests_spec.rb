f8c70bdeeaa7bacf487a5aa04d40a06f9db2484d
Michael Kirk <michael.john.kirk@gmail.com>
2015-03-16 18:25:19 -0700

    test real (locally) uploaded photo

--- spec/requests/api/v1/games_requests_spec.rb
+++ spec/requests/api/v1/games_requests_spec.rb
@@ -4,7 +4,8 @@ describe 'spots requests' do
   describe 'GET /api/v1/games/1/current_spot' do
     context 'with a spot' do
       before do
-        load Rails.root + 'db/seeds.rb'
+        require Rails.root + 'db/seeds.rb'
+        Seeds.seed!
       end
 
       it 'returns the current spot' do
@@ -19,12 +20,20 @@ describe 'spots requests' do
             id: 1,
             url: 'http://www.example.com/api/v1/games/1/spot/1.json',
             #user_id: 456, #TODO not implented yet.
-            image_url: '/images/medium/missing.png', #TODO implement photo upload
-            created_at: spot.created_at
+            image_url: 'http://localhost:3000//spots/images/000/000/001/medium/952_lucile.jpg?1426555184',
+            created_at: Seeds.lucile_spot.created_at
           }
         }.to_json)
 
+        # Image URL has to be checked separately
+        expected_image_url = expected_response['spot'].delete('image_url')
+        actual_image_url = actual_response['spot'].delete('image_url')
+
         expect(actual_response["spot"]).to eq(expected_response["spot"])
+
+        actual_image_url_without_query_parameters = actual_image_url.split("?")[0]
+        expected_image_url_without_query_parameters = expected_image_url.split("?")[0]
+        expect(actual_image_url_without_query_parameters).to match(expected_image_url_without_query_parameters)
       end
     end
   end
