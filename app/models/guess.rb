90eaa63c9bafc46831ebd6b7c71dc3ba6f1b8121
Michael Kirk & Imad Baghdady <michael.john.kirk@gmail.com>
2015-03-31 16:21:27 -0700

    API includes guess distance

--- app/models/guess.rb
+++ app/models/guess.rb
@@ -18,13 +18,17 @@ class Guess < ActiveRecord::Base
   end
 
   def close_enough?
-    raise RuntimeError.new("attempting to compare to non-existant spot") unless spot.present?
-    raise RuntimeError.new("attempting to compare to empty location") unless spot.location.present? && self.location.present?
-
     # This distance was generated experimentally. It's about 250 feet.
     # I have no idea what unit this measurement could possibly be in.
     # Some gentle research says it's in "whatever unit your projection is in"
-    self[:location].distance(spot[:location]) < 0.001
+    distance < 0.001
+  end
+
+  def distance
+    raise RuntimeError.new("attempting to compare to non-existant spot") unless spot.present?
+    raise RuntimeError.new("attempting to compare to empty location") unless spot.location.present? && self.location.present?
+
+    self[:location].distance(spot[:location])
   end
 
   def location=(val)
