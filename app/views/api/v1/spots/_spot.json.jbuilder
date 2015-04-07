5f31cf8a19c31bc8845aa1f2c05c78f158a23bc8
Michael Kirk & Imad Baghdady <michael.john.kirk@gmail.com>
2015-04-06 21:12:21 -0700

    bring spec up to spec

--- app/views/api/v1/spots/_spot.json.jbuilder
+++ app/views/api/v1/spots/_spot.json.jbuilder
@@ -1,5 +1,5 @@
 json.extract! spot, :id, :created_at, :image_url, :location
-json.url api_v1_spot_url(1, spot, format: :json)
+json.url api_v1_spot_url(spot, format: :json)
 json.guess_ids spot.guesses.map &:id
 json.user_id 1
 json.user_name "michael"
--- spec/requests/api/v1/spot_requests_spec.rb
+++ spec/requests/api/v1/spot_requests_spec.rb
@@ -1,13 +1,13 @@
 require 'rails_helper'
 
 describe 'spot requests' do
-  describe 'POST /api/v1/games/1/spots' do
+  describe 'POST /api/v1/spots' do
     context 'with a set up game' do
       before do
         require Rails.root + 'db/seeds.rb'
         Seeds.seed!
       end
-      let(:spot_id) { Seeds.lucile_spot.id + 1}
+      let(:last_spot) { Spot.last }
 
       it 'creates a new spot' do
 
@@ -15,27 +15,34 @@ describe 'spot requests' do
 
         spot_parameters = {
           spot: {
+            game_id: 1,
             location: {
               type: 'Point',
               coordinates: [-118.3240, 34.0937]
             },
-            image: image_data
+            image_data: image_data
           }
         }
 
-        post '/api/v1/games/1/spots.json', spot_parameters
+        post '/api/v1/spots', spot_parameters
         expect(response).to be_success
 
         actual_response = JSON.parse(response.body)
 
         expected_response = JSON.parse({
           spot:  {
-            id: spot_id,
-            url: "http://www.example.com/api/v1/games/1/spots/#{spot_id}.json",
+            id: last_spot.id,
+            game_id: 1,
+            guess_ids: [],
+            url: "http://www.example.com/api/v1/spots/#{last_spot.id}",
+            location: {
+              type: 'Point',
+              coordinates: [-118.324, 34.0937]
+            },
             user_id: 1, #TODO user system not implented yet.
             user_name: "michael", #TODO user system not implented yet.
-            image_url: sprintf('http://localhost:3000/uploads/spots/images/000/000/%03d/medium/952_lucile.jpg?1426555184', spot_id),
-            created_at: Spot.last.created_at
+            image_url: sprintf('http://localhost:3000/uploads/spots/images/000/000/%03d/medium/952_lucile.jpg?1426555184', last_spot.id),
+            created_at: last_spot.created_at
           }
         }.to_json)
 
