5b6e661211fbac71836b292be4877c2949c7fc37
Michael Kirk & Imad Baghdady <michael.john.kirk+michael.john.kirk@gmail.com>
2015-05-15 18:31:20 -0700

    user spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    describe 'password' do
+      it 'does not require a password unless there is an email' do
+        user = User.new
+        expect(user).to be_valid
+        user.email = "foo@example.com"
+        expect(user).to_not be_valid
+        user.password = "my-very-super-secret-password"
+        expect(user).to be_valid
+      end
+    end
+  end
 end
