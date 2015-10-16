3b8842037017b7a6719b48da672e37c7e06311df
Michael Kirk <michael.john.kirk@gmail.com>
2015-10-16 14:29:46 -0700

    https for images

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -18,7 +18,8 @@ else
     s3_credentials: {
       bucket: ENV['AWS_S3_BUCKET'],
       access_key_id: ENV['AWS_ACCESS_KEY_ID'],
-      secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
+      secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+      s3_protocol: 'https'
     }
   }
 end
