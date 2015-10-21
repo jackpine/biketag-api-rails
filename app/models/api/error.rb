23b08cd46d74310fd22e75b7f846a719eba485d2
Michael Kirk <michael.john.kirk@gmail.com>
2015-10-21 16:03:50 -0700

    get rid of unneccesary hash middleman

--- app/models/api/error.rb
+++ app/models/api/error.rb
@@ -1,15 +1,6 @@
 module Api::Error
   class ErrorBuilder
-    ERROR_MESSAGES = {
-      32 => 'Could not authenticate you.',
-      133 => 'Invalid Record.',
-      179 => 'You are not authorized to do that.'
-    }
-
-    def self.build(code)
-      default_message = ERROR_MESSAGES[code]
-      raise Exception.new("unknown error code: #{code}") if default_message.blank?
-
+    def self.build(code, default_message)
       error_class = Class.new(StandardError) do
         attr_accessor :message
 
@@ -21,18 +12,20 @@ module Api::Error
           { error: { message: message, code: code }}
         end
       end
-      error_class.send :define_method, :default_message do 
-        default_message
-      end
+
       error_class.send :define_method, :code do
         code
+      end
+      error_class.send :define_method, :default_message do
+        default_message
       end
 
       error_class
     end
   end
 
-  Unauthorized = ErrorBuilder.build(179)
-  InvalidRecord = ErrorBuilder.build(133)
-  Unauthenticated = ErrorBuilder.build(32)
+  Unauthenticated = ErrorBuilder.build(32, 'Could not authenticate you.')
+  InvalidRecord = ErrorBuilder.build(133, 'Invalid Record.')
+  Unauthorized = ErrorBuilder.build(179, 'You are not authorized to do that.')
+
 end
