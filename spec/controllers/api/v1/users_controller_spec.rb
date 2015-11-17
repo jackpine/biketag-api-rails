a43d2458d181de47e4a8f26a53725f68210d3a4a
Michael Kirk <michael.john.kirk@gmail.com>
2015-11-17 10:19:27 -0800

    update specs to reflect to user request format

--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe Api::V1::UsersController, type: :controller do
   end
 
   describe "#show" do
-    let(:user) { mock_model(User) }
+    let(:user) { mock_model(User, spots: [], guesses: [], games: []) }
     it 'shows user details' do
       expect(User).to receive(:find).with('2').and_return(user)
       get :show, id: 2, format: :json
