2a7ec0b08c7ea9511d581a2c8e435b13988ea26d
Michael Kirk <michael.john.kirk@gmail.com>
2016-05-11 21:55:50 -0700

    importing rails 5 config from `rails new test_api --api`

--- config/application.rb
+++ config/application.rb
@@ -1,5 +1,6 @@
-require File.expand_path('../boot', __FILE__)
+require_relative 'boot'
 
+require "rails"
 # Pick the frameworks you want:
 require "active_model/railtie"
 require "active_job/railtie"
