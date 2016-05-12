f4737ac3641c9c9c3c893f585c251dd84ddfd548
Michael Kirk <michael.john.kirk@gmail.com>
2016-05-12 08:51:12 -0700

    remove deprecated config

--- config/application.rb
+++ config/application.rb
@@ -29,8 +29,6 @@ module BikeTag
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
-    # Do not swallow errors in after_commit/after_rollback callbacks.
-    config.active_record.raise_in_transactional_callbacks = true
     config.host_uploads_locally = ENV['RAILS_HOST_UPLOADS_LOCALLY'] && ENV['RAILS_HOST_UPLOADS_LOCALLY'] != 'false'
 
     require 'rack/permissive-cors'
